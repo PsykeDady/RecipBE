41118414c7923e07e915d0557a20f6f8716aac69
PsykeDady <psdady@msn.com>
2022-05-16 09:30:11 +0200

    reorgiza imports
    	modificato:             src/main/java/psykeco/recipbe/db/RecipeQuery.java

--- src/main/java/psykeco/recipbe/db/RecipeQuery.java
+++ src/main/java/psykeco/recipbe/db/RecipeQuery.java
@@ -1,10 +1,9 @@
 package psykeco.recipbe.db;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import psykeco.recipbe.model.Ricetta;
 
+@Repository
 public interface RecipeQuery extends JpaRepository<Ricetta,String>{}
